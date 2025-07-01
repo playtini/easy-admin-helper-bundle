6a7ec11c3519d35c059726d04c4bfb3cab8ac7a5
Vova Luchaninov <vladimir.luchaninov@gmail.com>
2025-07-01 18:57:46 +0200

    DashboardCustomConstructorTrait

---
+++ src/Controller/Traits/DashboardCustomConstructorTrait.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Playtini\EasyAdminHelperBundle\Controller\Traits;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Playtini\EasyAdminHelperBundle\Dashboard\DashboardHelper;
+use Playtini\EasyAdminHelperBundle\Dashboard\EasyAdminMenuInterface;
+
+/**
+ * @property EasyAdminMenuInterface $easyAdminMenu
+ * @property DashboardHelper $dashboardHelper
+ */
+trait DashboardCustomConstructorTrait
+{
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle($this->easyAdminMenu->getTitle())
+            ->renderContentMaximized()
+        ;
+    }
+
+    public function configureCrud(): Crud
+    {
+        return $this->_configureCrud();
+    }
+
+    protected function _configureCrud(): Crud
+    {
+        return parent::configureCrud()
+            ->setDateFormat('yyyy-MM-dd')
+            ->setDateTimeFormat('yyyy-MM-dd HH:mm:ss')
+            ->renderContentMaximized()
+            ->setPaginatorPageSize(100)
+            ->showEntityActionsInlined()
+            ->setDefaultSort(['id' => 'DESC'])
+            ;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return $this->easyAdminMenu->configureMenuItems();
+    }
+
+    public function configureActions(): Actions
+    {
+        return Actions::new();
+    }
+
+    public function setMenuIndex(?string $key = null): void
+    {
+        $key ??= $this->dashboardHelper->requestStack->getCurrentRequest()?->getPathInfo();
+
+        $menuItems = $this->dashboardHelper->adminContextProvider->getContext()?->getMainMenu()->getItems();
+        $index = $this->easyAdminMenu->menuCounters[$key] ?? null;
+
+        if (null !== $index) {
+            $menuItems[$index]->setSelected(true);
+        }
+    }
+}
--- src/Controller/Traits/DashboardTrait.php
+++ src/Controller/Traits/DashboardTrait.php
@@ -4,63 +4,15 @@ declare(strict_types=1);
 
 namespace Playtini\EasyAdminHelperBundle\Controller\Traits;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use Playtini\EasyAdminHelperBundle\Dashboard\DashboardHelper;
 use Playtini\EasyAdminHelperBundle\Dashboard\EasyAdminMenuInterface;
 
 trait DashboardTrait
 {
+    use DashboardCustomConstructorTrait;
+
     public function __construct(
         protected EasyAdminMenuInterface $easyAdminMenu,
         protected DashboardHelper $dashboardHelper,
     ) {}
-
-    public function configureDashboard(): Dashboard
-    {
-        return Dashboard::new()
-            ->setTitle($this->easyAdminMenu->getTitle())
-            ->renderContentMaximized()
-        ;
-    }
-
-    public function configureCrud(): Crud
-    {
-        return $this->_configureCrud();
-    }
-
-    protected function _configureCrud(): Crud
-    {
-        return parent::configureCrud()
-            ->setDateFormat('yyyy-MM-dd')
-            ->setDateTimeFormat('yyyy-MM-dd HH:mm:ss')
-            ->renderContentMaximized()
-            ->setPaginatorPageSize(100)
-            ->showEntityActionsInlined()
-            ->setDefaultSort(['id' => 'DESC'])
-            ;
-    }
-
-    public function configureMenuItems(): iterable
-    {
-        return $this->easyAdminMenu->configureMenuItems();
-    }
-
-    public function configureActions(): Actions
-    {
-        return Actions::new();
-    }
-
-    public function setMenuIndex(?string $key = null): void
-    {
-        $key ??= $this->dashboardHelper->requestStack->getCurrentRequest()?->getPathInfo();
-
-        $menuItems = $this->dashboardHelper->adminContextProvider->getContext()?->getMainMenu()->getItems();
-        $index = $this->easyAdminMenu->menuCounters[$key] ?? null;
-
-        if (null !== $index) {
-            $menuItems[$index]->setSelected(true);
-        }
-    }
 }
