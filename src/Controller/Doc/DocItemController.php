f68de8de1ac5213dd6296553671e37097530cff6
Vova Luchaninov <vladimir.luchaninov@gmail.com>
2025-12-15 18:28:38 +0100

    allow customizing doc pages

--- src/Controller/Doc/DocItemController.php
+++ src/Controller/Doc/DocItemController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/admin/doc/{name}', name: 'admin_doc_item')]
-class DocDbItemController extends AbstractController
+class DocItemController extends AbstractController
 {
     public function __construct(
         private readonly EasyAdminContext $easyAdminContext,
